a07fccf12e280ae9a5b765169fdc2fa050a525ac
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-12-10 19:43:58 +0530

    change some types

--- common/dist/index.js
+++ common/dist/index.js
@@ -5,13 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.updatePostInput = exports.creatPostInput = exports.signinInput = exports.signupInput = void 0;
 const zod_1 = __importDefault(require("zod"));
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 exports.signupInput = zod_1.default.object({
-    username: zod_1.default.string().email(),
-    password: zod_1.default.string().min(6),
+    email: zod_1.default
+        .string()
+        .email("Please enter a valid email address")
+        .refine((val) => emailRegex.test(val), {
+        message: "Email must match the pattern: example@domain.com",
+    }),
+    password: zod_1.default.string().min(6, "Password must be at least 6 characters long"),
     name: zod_1.default.string().optional(),
 });
 exports.signinInput = zod_1.default.object({
-    username: zod_1.default.string().email(),
+    email: zod_1.default.string().email(),
     password: zod_1.default.string().min(6),
 });
 exports.creatPostInput = zod_1.default.object({
@@ -19,7 +25,7 @@ exports.creatPostInput = zod_1.default.object({
     content: zod_1.default.string(),
 });
 exports.updatePostInput = zod_1.default.object({
-    title: zod_1.default.string(),
-    content: zod_1.default.string(),
-    id: zod_1.default.number(),
+    title: zod_1.default.string().optional(),
+    content: zod_1.default.string().optional(),
+    id: zod_1.default.string(),
 });
