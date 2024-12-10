5466e23800c95239ab7e71c2fe2f7702493612d4
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-12-10 19:46:38 +0530

    add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,25 @@ import { Signin } from "./pages/Signin";
 import { Blogs } from "./pages/Blogs";
 import { Blog } from "./pages/Blog";
 import { Publish } from "./pages/Publish";
+import { UpdateBlog } from "./pages/UpdateBlog";
+import { GenerativeAi } from "./pages/GenAi";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Blogs />}></Route>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
+
           <Route path="/blog/:id" element={<Blog />} />
+
           <Route path="/blogs" element={<Blogs />} />
+
           <Route path="/publish" element={<Publish />} />
+          <Route path="/update/:id" element={<UpdateBlog />} />
+          <Route path="/geneartiveAi" element={<GenerativeAi />} />
         </Routes>
       </BrowserRouter>
     </>
